c58bc08fc1fb80be4dc439beae0f42d593a13d55
Abhie-Tripathi <abhiiii.tripathi@gmail.com>
2023-03-22 13:18:49 +0530

    semi-colon add

--- dom.js
+++ dom.js
@@ -1,6 +1,6 @@
 const header = document.querySelector("#main-header");
 header.style.borderBottom = "10px solid black"
 const childheader= document.getElementsByTagName("h2");
-childheader[0].style.color= "green"
-childheader[0].innerHTML="<b>Add Items</b>"
+childheader[0].style.color= "green";
+childheader[0].innerHTML="<b>Add Items</b>";
 
