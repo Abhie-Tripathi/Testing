e3032061bd52bef54c69c229ccb773f278578d02
Abhie-Tripathi <abhiiii.tripathi@gmail.com>
2023-03-22 13:57:08 +0530

    Get Elements byClassnames

--- dom.js
+++ dom.js
@@ -3,4 +3,8 @@ header.style.borderBottom = "10px solid black"
 const childheader= document.getElementsByTagName("h2");
 childheader[0].style.color= "green";
 childheader[0].innerHTML="<b>Add Items</b>";
-
+const list = document.querySelectorAll(".list-group-item");
+list[2].style.backgroundColor="green"
+for(i=0;i<list.length;i++){
+    list[i].style.fontWeight="bold"
+}
