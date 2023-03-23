bb9fdcef5755df421b7736e267066eb213303d0e
Abhie-Tripathi <abhiiii.tripathi@gmail.com>
2023-03-23 11:52:19 +0530

    Delete and Edit functionality

--- dom.js
+++ dom.js
@@ -1,58 +1,26 @@
-// parentElement
-// listitem=document.querySelector("#header-title");
-// console.log(listitem.parentElement)
-// listitem.parentElement.style.backgroundColor="red"
-// lastelementchild
-// listitem=document.querySelector("ul");
-// console.log(listitem.lastElementChild)
-// listitem.lastElementChild.style.backgroundColor="red"
-// lastChild
-// listitem=document.querySelector("ul");
-// console.log(listitem.lastChild) //logs basically the white spaces
-// listitem.lastChild.style.backgroundColor="red"
-// firstElementChild
-// listitem=document.querySelector("ul");
-// console.log(listitem.firstElementChild)
-// listitem.firstElementChild.style.backgroundColor="red"
-// firstChild
-// listitem=document.querySelector("ul");
-// console.log(listitem.firstChild)//logs the white spaces 
-// listitem.firstChild.style.backgroundColor="red"
-// nextSiblings
-// listitem=document.querySelector("h2");
-// console.log(listitem.nextSibling)//white spaces
-// listitem.nextSibling.style.color="red"
-//nextElementSibling
-// listitem=document.querySelector("h2");
-// console.log(listitem.nextElementSibling)
-// listitem.nextElementSibling.style.backgroundColor="red"
-// previousSibling
-// listitem=document.querySelector("ul");
-// console.log(listitem.previousElementSibling)
-// listitem.previousElementSibling.style.backgroundColor="red"
-// create Element
-const newdiv=document.createElement("div");
-newdiv.className = "div"
-newdiv.id="div1"
-// setAttribute
-newdiv.setAttribute('title',"attribute");
-// create text node
-const divtext = document.createTextNode("Hello");
-// append text node to element
-newdiv.appendChild(divtext)
-// selecting where to insert
-const cont = document.querySelector("header .container");
-// selecting Element to be placed after
-const h = document.querySelector("header h1");
-// inserting newdiv before h
-cont.insertBefore(newdiv,h);
-const newdiv1=document.createElement("div");
-const divtext1 = document.createTextNode("Hello");
-newdiv1.appendChild(divtext1)
-
-const list = document.querySelector("#items");
-
-const listitem1=document.querySelector("li");
-
-list.insertBefore(newdiv1,listitem1);
-
+const form = document.getElementById("addForm");
+const newlist = document.getElementById("items");
+form.addEventListener("submit", additem);
+function additem(e){
+    e.preventDefault();
+    const item = document.getElementById('item').value;
+    const list = document.createElement('li');
+    const edit = document.createElement("button");
+    edit.className="btn btn-primary btn-sm float-right delete"
+    edit.appendChild(document.createTextNode('Edit'))
+    list.appendChild(edit)
+    const btn = document.createElement('button');
+    btn.className="btn btn-danger btn-sm float-right delete"
+    btn.appendChild(document.createTextNode('x'))
+    list.className="list-group-item"
+    list.appendChild(document.createTextNode(item))
+    list.appendChild(btn)
+    newlist.appendChild(list)
+}
+newlist.addEventListener("click", deleteitem);
+function deleteitem(e){
+    if(e.target.classList.contains('delete')){
+        let rli = e.target.parentElement
+        newlist.removeChild(rli)
+    }
+}
