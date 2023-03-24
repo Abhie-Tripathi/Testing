1ae27c2a330fdd7d8c825c7fbf1ab65b9bede26a
Abhie-Tripathi <abhiiii.tripathi@gmail.com>
2023-03-24 17:07:48 +0530

    Lets Scale the app to more users

--- dom.js
+++ dom.js
@@ -1,66 +1,19 @@
-const form = document.getElementById("addForm");
-const newlist = document.getElementById("items");
-const desciption = document.createElement("input");
-desciption.className="form-control mr-2";
-desciption.id= "description";
-desciption.setAttribute("type","text");
-form.insertBefore(desciption, form.lastElementChild);
-
-
-form.addEventListener("submit", additem);
-function additem(e){
+const username = document.getElementById("name");
+const email = document.getElementById("email");
+const phone= document.getElementById("phone");
+const form = document.getElementById("my-form");
+const ul = document.getElementById("users")
+form.addEventListener("submit", addinlocal);
+function addinlocal(e){
     e.preventDefault();
-    const item = document.getElementById('item').value;
-    const list = document.createElement('li');
-    const btn = document.createElement('button');
-    btn.className="btn btn-danger btn-sm float-right delete"
-    btn.appendChild(document.createTextNode('x'))
-    list.className="list-group-item"
-    list.appendChild(document.createTextNode(item))
+    var details ={"'username":username.value,"email":email.value,"phone":phone.value};
+    var strdetails = JSON.stringify(details)
+    localStorage.setItem(username.value, strdetails);
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(`Name = ${username.value} Email = ${email.value} 
+    Phone No = ${phone.value}`))
+    ul.appendChild(li)
+    var details =""
     
-    li = document.createElement("br")
-    list.appendChild(li)
-    list.appendChild(document.createTextNode(desciption.value))
-    
-    
-    
-    list.appendChild(btn)
-    const edit = document.createElement("button");
-    edit.className="btn btn-primary btn-sm float-right"
-    edit.appendChild(document.createTextNode('Edit'))
-    list.appendChild(edit)
-    newlist.appendChild(list)
-}
-newlist.addEventListener("click", deleteitem);
-function deleteitem(e){
-    if(e.target.classList.contains('delete')){
-        let rli = e.target.parentElement
-        newlist.removeChild(rli)
-    }
-}
-const filter = document.getElementById("filter");
-filter.addEventListener("keyup",filteritems);
-function filteritems(e){
-    var text = e.target.value.toLowerCase()
-    var items = newlist.getElementsByTagName("li");
-    console.log(items)
-    Array.from(items).forEach(function(item){
-        var itemname = item.firstChild.textContent;
-        if(item.childNodes[2]!=undefined){
-            var itemnode = item.childNodes[2].textContent
-        }
-        else{
-            var itemnode = ""
-        }
-        console.log(itemnode)
-        console.log(itemname)
-    
-        if(itemname.toLowerCase().indexOf(text) != -1 || itemnode.toLowerCase().indexOf(text) != -1){
-            item.style.display = 'block';
-        }
-        else{
-            item.style.display= 'none'
-        }
-     } )}
 
-
+}
