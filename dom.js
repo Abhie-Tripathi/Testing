6843aed53e0e71499ca26259dc055ac4d86f426a
Abhie-Tripathi <abhiiii.tripathi@gmail.com>
2023-03-24 11:11:51 +0530

    Lets store the users in the right format

--- dom.js
+++ dom.js
@@ -1,10 +1,14 @@
-const username = document.getElementById("name");
-const email = document.getElementById("email");
-const form = document.getElementById("my-form");
-form.addEventListener("submit", addinlocal);
-function addinlocal(e){
-    localStorage.setItem("name",username.value);
-    localStorage.setItem("email",email.value);
-}
-
-
+// const username = document.getElementById("name");
+// const email = document.getElementById("email");
+// const form = document.getElementById("my-form");
+// form.addEventListener("submit", addinlocal);
+// function addinlocal(e){
+//     localStorage.setItem("name",username.value);
+//     localStorage.setItem("email",email.value);
+// }
+var obj={user:"Abhinav",email:"Abhiiii.tripathi@gmail.com"}
+var stringobj=JSON.stringify(obj);
+localStorage.setItem("obj", stringobj);
+console.log(localStorage.getItem("obj"));
+var destingobh = JSON.parse(localStorage.getItem("obj"));
+console.log(destingobh)
