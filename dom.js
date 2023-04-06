c27668fd070a017c80cacf1387c6867b5af7ea37
Abhie-Tripathi <abhiiii.tripathi@gmail.com>
2023-04-06 09:47:35 +0530

    fectching data from server when dom is loaded

--- dom.js
+++ dom.js
@@ -6,33 +6,35 @@ const ul = document.getElementById("users")
 form.addEventListener("submit", addinlocal);
 function addinlocal(e) {
     e.preventDefault();
+
     var obj = { "username": username.value, "email": email.value, "phone": phone.value };
-    // Calling post request on crucrud api
+    
     axios.post("https://crudcrud.com/api/54a2404bdf944e6fa66a4b648c25abf6/appointments",obj)
+    
+    showonscreen(obj)
+}
 
-    var li = document.createElement("li");
-    var btn = document.createElement("button");
-    var edit = document.createElement("button");
-    edit.className= "edit"
-    edit.appendChild(document.createTextNode("Edit"));
-    btn.className = "delete"
-    btn.appendChild(document.createTextNode("Delete"))
-    li.appendChild(document.createTextNode(`Name = ${username.value} Email = ${email.value} 
-    Phone No = ${phone.value}`))
-    li.appendChild(edit)
-    li.append(btn)
-    ul.appendChild(li)
-    // btn.onclick = () => {
-    //     localStorage.removeItem(obj.username)
-    //     ul.removeChild(li)
-    // }
-    edit.addEventListener("click",r);
-    function r(e){
-        e.preventDefault()
-        if(e.target.className="edit"){
-        localStorage.removeItem(obj.username);
-        username.value=obj.username
-        email.value= obj.email
-        phone.value= obj.phone}}
+function showonscreen(obj){
+            var li = document.createElement("li");
+            var btn = document.createElement("button");
+            var edit = document.createElement("button");
+            edit.className= "edit"
+            edit.appendChild(document.createTextNode("Edit"));
+            btn.className = "delete"
+            btn.appendChild(document.createTextNode("Delete"))
+            li.appendChild(document.createTextNode(`Name = ${obj.username} Email = ${obj.email} 
+            Phone No = ${obj.phone}`))
+            li.appendChild(edit)
+            li.append(btn)
+            ul.appendChild(li)}
 
+
+
+window.addEventListener("DOMContentLoaded",()=> {
+    axios.get("https://crudcrud.com/api/54a2404bdf944e6fa66a4b648c25abf6/appointments")
+    .then(data => {
+        for(let i=0;i<data.data.length;i++){
+            showonscreen(data.data[i])
         }
+    })
+})
